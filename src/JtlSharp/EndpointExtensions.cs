5aed5e06b47e9e038ec1824bc4f118e2bb3c88f9
Burak Tamturk <buraktamturk@mail.ru>
2024-05-16 16:03:42 +0300

    stuff

--- src/JtlSharp/EndpointExtensions.cs
+++ src/JtlSharp/EndpointExtensions.cs
@@ -66,121 +66,127 @@ public static class EndpointExtensions
                 
                 try
                 {
+                    object result;
+                    
                     if (!form.TryGetValue("jtlauth", out var jtlauth)) {
                         switch (rpc.method)
                         {
+                            case "connector.identify":
+                                var service2 = ActivatorUtilities.CreateInstance<T>(context.RequestServices);
+
+                                result = new JtlRpcResult<object>
+                                {
+                                    jtlrpc = "2.0",
+                                    id = rpc.id,
+                                    result = await service2.Identify()
+                                };
+                            break;
                             case "core.connector.auth":
                                 var authentication = rpc.Read<Authentication>();
                                 if (token != authentication.token)
                                     throw new InvalidOperationException("Authentication failed.");
-                                
-                                await context.Response.WriteAsJsonAsync(new JtlRpcResult<Session>
-                                {
-                                    jtlrpc = "2.0",
-                                    id = rpc.id,
-                                    result = new Session
-                                    {
-                                        sessionId = Guid.NewGuid().ToString(),
-                                        lifetime = 3600
-                                    }
-                                });
 
-                                return;
+                                result = new Session
+                                {
+                                    sessionId = Guid.NewGuid().ToString(),
+                                    lifetime = 3600
+                                };
+                            break;
                             default:
                                 throw new InvalidOperationException("Not authenticated for action: " + rpc.method);
                         }
-                    }
-                    
-                    // token control
+                    } else {
+                        // token control
 
-                    await using var zipFileStream =  form.Files.Count > 0 ? form.Files[0].OpenReadStream() : null;
-                    using var zipFile = zipFileStream is null ? null : new ZipArchive(zipFileStream);
-                    
-                    var service = ActivatorUtilities.CreateInstance<T>(context.RequestServices);
-                    
-                    var features = service.GetFeatures();
-                    
-                    var result = rpc.method switch
-                    {
-                        "core.connector.features" => features,
-                        "core.connector.ack" => await features.Entities.ProcessAck(rpc.Read<Ack>()!),
-                        "core.connector.init" => true,
-                        "connector.finish" => true,
-                        "connector.identify" => await service.Identify(),
-                        "core.linker.clear" => rpc?.Read<Ack>() is {} ack
-                            ? await features.Entities.ProcessAck(ack, true)
-                            : await service.Clear(),
+                        await using var zipFileStream =  form.Files.Count > 0 ? form.Files[0].OpenReadStream() : null;
+                        using var zipFile = zipFileStream is null ? null : new ZipArchive(zipFileStream);
                         
-                        "globalData.pull" => new GlobalData()
+                        var service = ActivatorUtilities.CreateInstance<T>(context.RequestServices);
+                        
+                        var features = service.GetFeatures();
+                        
+                        result = rpc.method switch
                         {
-                            currencies = features.Entities.Currency?.Pull is null ? [] : await features.Entities.Currency.Pull(null).ToListAsync(),
-                            languages = features.Entities.Language?.Pull is null ? [] : await features.Entities.Language.Pull(null).ToListAsync(),
-                            warehouses = features.Entities.Warehouse?.Pull is null ? [] : await features.Entities.Warehouse.Pull(null).ToListAsync(),
-                            productTypes = features.Entities.ProductType?.Pull is null ? [] : await features.Entities.ProductType.Pull(null).ToListAsync(),
-                            shippingClasses = features.Entities.ShippingClass?.Pull is null ? [] : await features.Entities.ShippingClass.Pull(null).ToListAsync(),
-                            shippingMethods = features.Entities.ShippingMethod?.Pull is null ? [] : await features.Entities.ShippingMethod.Pull(null).ToListAsync(),
-                            crossSellingGroups = features.Entities.CrossSellingGroup?.Pull is null ? [] : await features.Entities.CrossSellingGroup.Pull(null).ToListAsync(),
-                            taxRates = features.Entities.TaxRate?.Pull is null ? [] : await features.Entities.TaxRate.Pull(null).ToListAsync(),
-                            units = features.Entities.Unit?.Pull is null ? [] : await features.Entities.Unit.Pull(null).ToListAsync(),
-                            configGroups = features.Entities.ConfigGroup?.Pull is null ? [] : await features.Entities.ConfigGroup.Pull(null).ToListAsync(),
-                            configItems = features.Entities.ConfigItem?.Pull is null ? [] : await features.Entities.ConfigItem.Pull(null).ToListAsync(),
-                            customerGroups = features.Entities.CustomerGroup?.Pull is null ? [] : await features.Entities.CustomerGroup.Pull(null).ToListAsync(),
-                            measurementUnits = features.Entities.MeasurementUnit?.Pull is null ? [] : await features.Entities.MeasurementUnit.Pull(null).ToListAsync(),
-                        },
+                            "core.connector.features" => features,
+                            "core.connector.ack" => await features.Entities.ProcessAck(rpc.Read<Ack>()!),
+                            "core.connector.init" => true,
+                            "connector.finish" => true,
+                            "connector.identify" => await service.Identify(),
+                            "core.linker.clear" => rpc?.Read<Ack>() is {} ack
+                                ? await features.Entities.ProcessAck(ack, true)
+                                : await service.Clear(),
+                            
+                            "globalData.pull" => new GlobalData()
+                            {
+                                currencies = features.Entities.Currency?.Pull is null ? [] : await features.Entities.Currency.Pull(null).ToListAsync(),
+                                languages = features.Entities.Language?.Pull is null ? [] : await features.Entities.Language.Pull(null).ToListAsync(),
+                                warehouses = features.Entities.Warehouse?.Pull is null ? [] : await features.Entities.Warehouse.Pull(null).ToListAsync(),
+                                productTypes = features.Entities.ProductType?.Pull is null ? [] : await features.Entities.ProductType.Pull(null).ToListAsync(),
+                                shippingClasses = features.Entities.ShippingClass?.Pull is null ? [] : await features.Entities.ShippingClass.Pull(null).ToListAsync(),
+                                shippingMethods = features.Entities.ShippingMethod?.Pull is null ? [] : await features.Entities.ShippingMethod.Pull(null).ToListAsync(),
+                                crossSellingGroups = features.Entities.CrossSellingGroup?.Pull is null ? [] : await features.Entities.CrossSellingGroup.Pull(null).ToListAsync(),
+                                taxRates = features.Entities.TaxRate?.Pull is null ? [] : await features.Entities.TaxRate.Pull(null).ToListAsync(),
+                                units = features.Entities.Unit?.Pull is null ? [] : await features.Entities.Unit.Pull(null).ToListAsync(),
+                                configGroups = features.Entities.ConfigGroup?.Pull is null ? [] : await features.Entities.ConfigGroup.Pull(null).ToListAsync(),
+                                configItems = features.Entities.ConfigItem?.Pull is null ? [] : await features.Entities.ConfigItem.Pull(null).ToListAsync(),
+                                customerGroups = features.Entities.CustomerGroup?.Pull is null ? [] : await features.Entities.CustomerGroup.Pull(null).ToListAsync(),
+                                measurementUnits = features.Entities.MeasurementUnit?.Pull is null ? [] : await features.Entities.MeasurementUnit.Pull(null).ToListAsync(),
+                            },
 
-                        var s when s.StartsWith("product.") 
-                            => await features.Entities.Product.Process(rpc),
-                        var s when s.StartsWith("category.") 
-                            => await features.Entities.Category.Process(rpc),
-                        var s when s.StartsWith("manufacturer.") 
-                            => await features.Entities.Manufacturer.Process(rpc),
-                        var s when s.StartsWith("currency.") 
-                            => await features.Entities.Currency.Process(rpc),
-                        var s when s.StartsWith("customer.") 
-                            => await features.Entities.Customer.Process(rpc),
-                        var s when s.StartsWith("customerGroup.") 
-                            => await features.Entities.CustomerGroup.Process(rpc),
-                        var s when s.StartsWith("customerOrder.") 
-                            => await features.Entities.CustomerOrder.Process(rpc),
-                        var s when s.StartsWith("deliveryNote.") 
-                            => await features.Entities.DeliveryNote.Process(rpc),
-                        var s when s.StartsWith("fileDownload.") 
-                            => await features.Entities.FileDownload.Process(rpc),
-                        var s when s.StartsWith("fileUpload.") 
-                            => await features.Entities.FileUpload.Process(rpc),
-                        var s when s.StartsWith("image.") 
-                            => await features.Entities.Image.Process(rpc),
-                        var s when s.StartsWith("language.") 
-                            => await features.Entities.Language.Process(rpc),
-                        var s when s.StartsWith("measurementUnit.") 
-                            => await features.Entities.MeasurementUnit.Process(rpc),
-                        var s when s.StartsWith("mediaFile.") 
-                            => await features.Entities.MediaFile.Process(rpc),
-                        var s when s.StartsWith("partsList.") 
-                            => await features.Entities.PartsList.Process(rpc),
-                        var s when s.StartsWith("payment.") 
-                            => await features.Entities.Payment.Process(rpc),
-                        var s when s.StartsWith("productPrice.") 
-                            => await features.Entities.ProductPrice.Process(rpc),
-                        var s when s.StartsWith("productStockLevel.") 
-                            => await features.Entities.ProductStockLevel.Process(rpc),
-                        var s when s.StartsWith("specific.") 
-                            => await features.Entities.Specific.Process(rpc),
-                        var s when s.StartsWith("statusChange.") 
-                            => await features.Entities.StatusChange.Process(rpc),
-                        var s when s.StartsWith("taxClass.") 
-                            => await features.Entities.TaxClass.Process(rpc),
-                        var s when s.StartsWith("taxRate.") 
-                            => await features.Entities.TaxRate.Process(rpc),
-                        var s when s.StartsWith("taxZone.") 
-                            => await features.Entities.TaxZone.Process(rpc),
-                        var s when s.StartsWith("unit.") 
-                            => await features.Entities.Unit.Process(rpc),
-                        var s when s.StartsWith("warehouse.") 
-                            => await features.Entities.Warehouse.Process(rpc),
+                            var s when s.StartsWith("product.") 
+                                => await features.Entities.Product.Process(rpc),
+                            var s when s.StartsWith("category.") 
+                                => await features.Entities.Category.Process(rpc),
+                            var s when s.StartsWith("manufacturer.") 
+                                => await features.Entities.Manufacturer.Process(rpc),
+                            var s when s.StartsWith("currency.") 
+                                => await features.Entities.Currency.Process(rpc),
+                            var s when s.StartsWith("customer.") 
+                                => await features.Entities.Customer.Process(rpc),
+                            var s when s.StartsWith("customerGroup.") 
+                                => await features.Entities.CustomerGroup.Process(rpc),
+                            var s when s.StartsWith("customerOrder.") 
+                                => await features.Entities.CustomerOrder.Process(rpc),
+                            var s when s.StartsWith("deliveryNote.") 
+                                => await features.Entities.DeliveryNote.Process(rpc),
+                            var s when s.StartsWith("fileDownload.") 
+                                => await features.Entities.FileDownload.Process(rpc),
+                            var s when s.StartsWith("fileUpload.") 
+                                => await features.Entities.FileUpload.Process(rpc),
+                            var s when s.StartsWith("image.") 
+                                => await features.Entities.Image.Process(rpc),
+                            var s when s.StartsWith("language.") 
+                                => await features.Entities.Language.Process(rpc),
+                            var s when s.StartsWith("measurementUnit.") 
+                                => await features.Entities.MeasurementUnit.Process(rpc),
+                            var s when s.StartsWith("mediaFile.") 
+                                => await features.Entities.MediaFile.Process(rpc),
+                            var s when s.StartsWith("partsList.") 
+                                => await features.Entities.PartsList.Process(rpc),
+                            var s when s.StartsWith("payment.") 
+                                => await features.Entities.Payment.Process(rpc),
+                            var s when s.StartsWith("productPrice.") 
+                                => await features.Entities.ProductPrice.Process(rpc),
+                            var s when s.StartsWith("productStockLevel.") 
+                                => await features.Entities.ProductStockLevel.Process(rpc),
+                            var s when s.StartsWith("specific.") 
+                                => await features.Entities.Specific.Process(rpc),
+                            var s when s.StartsWith("statusChange.") 
+                                => await features.Entities.StatusChange.Process(rpc),
+                            var s when s.StartsWith("taxClass.") 
+                                => await features.Entities.TaxClass.Process(rpc),
+                            var s when s.StartsWith("taxRate.") 
+                                => await features.Entities.TaxRate.Process(rpc),
+                            var s when s.StartsWith("taxZone.") 
+                                => await features.Entities.TaxZone.Process(rpc),
+                            var s when s.StartsWith("unit.") 
+                                => await features.Entities.Unit.Process(rpc),
+                            var s when s.StartsWith("warehouse.") 
+                                => await features.Entities.Warehouse.Process(rpc),
 
-                        _ => throw new InvalidOperationException("Method not found: " + rpc.method)
-                    };
+                            _ => throw new InvalidOperationException("Method not found: " + rpc.method)
+                        };
+                    }
 
                     var jtlResponseObj = new JtlRpcResult<object>
                     {
